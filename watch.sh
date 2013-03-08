5dc4d5a4d13ec627aeff54d99cca7ca1135ea8bf
madx <madx+github@yapok.org>
2013-03-08 23:18:58 +0100

    Make the watch script a bit more verbose

--- watch.sh
+++ watch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-inotifywait -qrm src/ media/ Rakefile template.haml meta.yml -e CLOSE_WRITE | 
+inotifywait -rm src/ media/ Rakefile template.haml meta.yml -e CLOSE_WRITE |
   while read line; do
     bundle exec rake
   done
