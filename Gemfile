5193e34932099d5f0ee7a879211abbc5decd6bf7
madx <madx+github@yapok.org>
2014-05-30 21:19:30 +0200

    REMOVE ALL THE THINGS

--- Gemfile
+++
@@ -1,7 +0,0 @@
-source 'https://rubygems.org/'
-
-gem "rake"
-gem "haml"
-gem "redcarpet"
-gem "rouge"
-gem "sass"
--- watch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-inotifywait -rm src/ static/ Rakefile templates -e CLOSE_WRITE |
-  while read line; do
-    bundle exec rake
-  done
